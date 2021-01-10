156bb0d60d91aff01aa094765a893db89bf20850
Petr Fenster <77027099+petrfenster@users.noreply.github.com>
2021-01-09 16:33:37 -0800

    Add files via upload

--- script.py
+++ script.py
@@ -1,27 +1,7 @@
-#!/usr/bin/env python
-
 import requests
+
 import json
 
+response = json.loads(requests.get("https://eonet.sci.gsfc.nasa.gov/api/v3/events").text)
 
-api_token = 'qm1SSKPqmNyo9OMVcgf05MxGGsG04hZGnOKEk2ED'
-api_url_base =  'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events'
-#api_url_base = 'https://api.digitalocean.com/v2/'
-
-headers = {'Content-Type': 'application/json',
-           'Authorization': 'Bearer {0}'.format(api_token)}
-
-def get_account_info():
-
-    api_url = '{0}account'.format(api_url_base)
-
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        print(json.loads(response.content.decode('utf-8')))
-        return json.loads(response.content.decode('utf-8'))
-    else:
-        return None
-
-data = get_account_info()
-print(data)
+print (response)
