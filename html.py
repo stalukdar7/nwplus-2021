b210039eeeedc045cedfc49d44f656f23d8968d8
Shaon Talukdar <shaontal@students.cs.ubc.ca>
2021-01-09 21:24:58 -0700

    Update html.py

--- html.py
+++ html.py
@@ -4,7 +4,19 @@ f = open('output.html','wb')
 
 message = """<html>
 <head></head>
-<body><p>Output</p></body>
+<body>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Location</th>
+    <th>Coordinates</th>
+  </tr>
+  <tr>
+  </tr>
+  <tr>
+  </tr>
+</table>
+</body>
 </html>"""
 
 f.write(message)
